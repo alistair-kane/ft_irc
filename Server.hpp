b0ef73f44768f4cc1a412a1a98a906263713c4d4
Tim <tgerdes@student.42wolfsburg.de>
2022-09-25 21:42:48 +0200

    Added functions for server commands

--- Server.hpp
+++ Server.hpp
@@ -105,4 +105,36 @@ class Server
 		// void					store_message(int const & fd, char const * input);
 		// void					remove_message(int const & fd);
 
+		// Server command functions
+		int	exec_cmd_ADMIN(Message &cmd_msg);
+		int	exec_cmd_AWAY(Message &cmd_msg);
+		int	exec_cmd_INVITE(Message &cmd_msg);
+		int	exec_cmd_JOIN(Message &cmd_msg);
+		int	exec_cmd_KICK(Message &cmd_msg);
+		int	exec_cmd_KNOCK(Message &cmd_msg);
+		int	exec_cmd_LINKS(Message &cmd_msg);
+		int	exec_cmd_LIST(Message &cmd_msg);
+		int	exec_cmd_LUSERS(Message &cmd_msg);
+		int	exec_cmd_MAP(Message &cmd_msg);
+		int	exec_cmd_MODE(Message &cmd_msg);
+		int	exec_cmd_MOTD(Message &cmd_msg);
+		int	exec_cmd_NAMES(Message &cmd_msg);
+		int	exec_cmd_NICK(Message &cmd_msg);
+		int	exec_cmd_NOTICE(Message &cmd_msg);
+		int	exec_cmd_PART(Message &cmd_msg);
+		int	exec_cmd_PASS(Message &cmd_msg);
+		int	exec_cmd_PING(Message &cmd_msg);
+		int	exec_cmd_PONG(Message &cmd_msg);
+		int	exec_cmd_PRIVMSG(Message &cmd_msg);
+		int	exec_cmd_QUIT(Message &cmd_msg);
+		int	exec_cmd_RULES(Message &cmd_msg);
+		int	exec_cmd_SETNAME(Message &cmd_msg);
+		int	exec_cmd_SILENCE(Message &cmd_msg);
+		int	exec_cmd_STATS(Message &cmd_msg);
+		int	exec_cmd_USER(Message &cmd_msg);
+		int	exec_cmd_VERSION(Message &cmd_msg);
+		int	exec_cmd_WHO(Message &cmd_msg);
+		int	exec_cmd_WHOIS(Message &cmd_msg);
+		int	exec_cmd_WHOWAS(Message &cmd_msg);
+
 };
