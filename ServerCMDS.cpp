73088c1f95f5551542ae446357ccebc831f321ad
Tim <tgerdes@student.42wolfsburg.de>
2022-10-12 22:36:02 +0200

    Patch release from Budapest to Wolfsburg

--- ServerCMDS.cpp
+++ ServerCMDS.cpp
@@ -1,22 +1,22 @@
 #include "Server.hpp"
 
-// int	Server::exec_cmd_ADMIN(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_ADMIN(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_AWAY(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_AWAY(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_INVITE(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_INVITE(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
 void	Server::exec_cmd_JOIN(Message &cmd_msg)
 {
@@ -49,29 +49,29 @@ void	Server::exec_cmd_JOIN(Message &cmd_msg)
 	return ;
 }
 
-// int	Server::exec_cmd_KICK(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_KICK(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_KNOCK(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_KNOCK(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_LINKS(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_LINKS(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_LIST(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_LIST(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
 void	Server::exec_cmd_LUSERS(Message &cmd_msg)
 {
@@ -81,29 +81,29 @@ void	Server::exec_cmd_LUSERS(Message &cmd_msg)
 	return ;
 }
 
-// int	Server::exec_cmd_MAP(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_MAP(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_MODE(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_MODE(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_MOTD(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_MOTD(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_NAMES(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_NAMES(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
 // registering the session
 // !!!! need to handle changing the nickname
@@ -145,98 +145,98 @@ void	Server::exec_cmd_NICK(Message &cmd_msg)
 	// return (0);
 }
 
-// int	Server::exec_cmd_NOTICE(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_NOTICE(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_PART(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_PART(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_PASS(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_PASS(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_PING(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_PING(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_PONG(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_PONG(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_PRIVMSG(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_PRIVMSG(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_QUIT(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_QUIT(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_RULES(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_RULES(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_SETNAME(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_SETNAME(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_SILENCE(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_SILENCE(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_STATS(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_STATS(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_USER(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_USER(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_VERSION(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_VERSION(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_WHO(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_WHO(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_WHOIS(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_WHOIS(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
 
-// int	Server::exec_cmd_WHOWAS(Message &cmd_msg)
-// {
-// 	(void)cmd_msg;
-// 	return (0);
-// }
+void	Server::exec_cmd_WHOWAS(Message &cmd_msg)
+{
+	(void)cmd_msg;
+	return ;
+}
