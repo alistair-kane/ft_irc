ed5bf2bddb122ea876b7b51724c2ae4316e71dde
Alistair Kane <alistair@192-168-1-123.tpgi.com.au>
2023-04-03 23:52:15 +1000

    added own bot fact

--- ServerBot.cpp
+++ ServerBot.cpp
@@ -4,15 +4,20 @@ void	Server::bot_check(int fd, Message const &msg)
 {
     std::string content = msg.raw_msg();
     // maybe make vector of random facts xd
-    char const *raf = "random alistair fact";
+    char const *ranf = "random alistair fact";
     char const *rtf = "random tim fact";
+    char const *ralf = "Alistair likes to eat peanut butter from the jar, like ice-cream"
 
 	if (content.find("alistair") != std::string::npos)
     {
-        send(fd, raf, strlen(raf), MSG_DONTWAIT);
+        send(fd, ralf, strlen(ralf), MSG_DONTWAIT);
     }
     else if (content.find("tim") != std::string::npos)
     {
         send(fd, rtf, strlen(rtf), MSG_DONTWAIT);
+    }
+    else if (content.find("anna") != std::string::npos)
+    {
+        send(fd, ranf, strlen(ranf), MSG_DONTWAIT);
     }
 }
